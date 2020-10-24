ff153c08fb524bbcdcf05f69085fb2c35fb11d5e
Masahiro Nitta <mnitta220@gmail.com>
2020-10-25 08:36:48 +0900

    initial commit

---
+++ build_pug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+pug index.pug --pretty
+pug deploy.pug --pretty
+pug develop.pug --pretty
+pug devsetup.pug --pretty
+pug devsetupMac.pug --pretty
+pug devsetupMysql.pug --pretty
+pug download.pug --pretty
+pug howtouse.pug --pretty

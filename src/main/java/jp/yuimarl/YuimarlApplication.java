285a0a97d877e7a1b22da56e6ad9e262079b65e0
Masahiro Nitta <mnitta220@gmail.com>
2020-10-25 08:26:53 +0900

    initial commit

---
+++ src/main/java/jp/yuimarl/YuimarlApplication.java
@@ -0,0 +1,13 @@
+package jp.yuimarl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuimarlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuimarlApplication.class, args);
+	}
+
+}
